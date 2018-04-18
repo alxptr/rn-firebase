e86377148edabdea1cd3242a060d4a89ba718585
alex.poter <a.poterenko@gmail.com>
2018-04-18 18:41:33 +0300

    react-native-camera

--- android/app/src/main/java/com/myweedstore/MainApplication.java
+++ android/app/src/main/java/com/myweedstore/MainApplication.java
@@ -3,6 +3,7 @@ package com.myweedstore;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNCameraPackage(),
           new RNFirebasePackage(),
           new RNFirebaseMessagingPackage(),
           new RNFirebaseNotificationsPackage(),
